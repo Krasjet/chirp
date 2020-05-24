026bfdbf2d233b08455900b4adc87e2cfa98d877
krasjet <nil@krj.st>
2020-05-24 19:38:04 -0400

    functions for parsing text

---
+++ src/Libkst/Text/Parse.hs
@@ -0,0 +1,116 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Libkst.Text.Parse (
+  skipTo,
+  skipAfter,
+  takeUntil,
+  between,
+  between',
+  betweenC,
+  betweenC',
+) where
+
+import qualified Data.Text as T
+
+import Data.Text (Text)
+
+-- | Skip anything before the first instance of prefix, used as a preprocessor
+-- for parsec. Parser combinators are not very good at this due to
+-- back tracking. Will return @""@ if prefix is not in the string.
+--
+-- @
+-- skipTo "<xml>" "<p><xml></xml>" == "<xml></xml>"
+-- @
+skipTo
+  :: Text -- ^ Prefix
+  -> Text -- ^ The string
+  -> Text -- ^ Text after prefix
+skipTo pfx = snd . T.breakOn pfx
+
+-- | Skip after the first instance of prefix, used as a preprocessor
+-- for parsec. Will return 'Nothing' if prefix is not in the string.
+--
+-- @
+-- skipAfter "\\begin{" "aaaa\\begin{env}bbbb" == Just "env}bbbb"
+-- skipAfter "\\begin{" "aaaa\\end{env}bbbb" == Nothing
+-- @
+skipAfter
+  :: Text       -- ^ Prefix
+  -> Text       -- ^ The string
+  -> Maybe Text -- ^ Text after prefix, or 'Nothing' if prefix not in string
+skipAfter pfx = T.stripPrefix pfx . skipTo pfx
+
+-- | Take until the first instance of suffix is found, used as preprocessor for
+-- parsec. Will return @""@ if prefix is not in the string.
+--
+-- @
+-- takeUntil "</xml>" "<p><xml></xml>" == "<p><xml>"
+-- @
+takeUntil
+  :: Text -- ^ Suffix
+  -> Text -- ^ The string
+  -> Text -- ^ Text before suffix
+takeUntil sfx = fst . T.breakOn sfx
+
+-- | Obtain the text between two delimiters. If prefix or suffix is not found, returns
+-- 'Nothing'.
+--
+-- @
+-- between "<xml>" "</xml>" "<xml>text</xml>" == Just "text"
+-- between "<xml>" "</xml>" "<xml>text<xml>"  == Nothing
+-- @
+between
+  :: Text -- ^ Begin delimiter
+  -> Text -- ^ End delimiter
+  -> Text -- ^ The string.
+  -> Maybe Text -- ^ Text between begin and end
+between beg end txt = do
+  sfx <- skipAfter beg txt
+  case T.breakOn end sfx of
+    (_, "")  -> Nothing
+    (pfx, _) -> return pfx
+
+-- | The same as 'between' except suffix delimiter is 'Char'. This might
+-- be slightly more efficient than 'between'.
+--
+-- @
+-- between "\\begin{" '}' "\\begin{env}" == Just "env"
+-- between "\\begin{" '}' "\\begin{env"  == Nothing
+-- @
+betweenC
+  :: Text       -- ^ Begin delimiter
+  -> Char       -- ^ End delimiter
+  -> Text       -- ^ The string.
+  -> Maybe Text -- ^ Text between begin and end
+betweenC beg end txt = do
+  sfx <- skipAfter beg txt
+  case T.break (==end) sfx of
+    (_, "")  -> Nothing
+    (pfx, _) -> return pfx
+
+-- | The same as 'between', but if prefix is found but suffix is not found,
+-- returns anything after prefix. Might be slightly more efficient.
+--
+-- @
+-- between "<xml>" "</xml>" "<xml>text</xml>" == Just "text"
+-- between "<xml>" "</xml>" "<xml>text<xml>"  == Just "text<xml>"
+-- @
+between'
+  :: Text       -- ^ Prefix delimiter
+  -> Text       -- ^ Suffix delimiter
+  -> Text       -- ^ The string.
+  -> Maybe Text -- ^ Text between begin and end
+between' beg end txt = skipAfter beg txt >>= (return . takeUntil end)
+
+-- | The same as 'between'' except suffix delimiter is a character. This might
+-- be slightly more efficient than 'between''.
+--
+-- @
+-- between "\\begin{" '}' "\\begin{env}" == Just "env"
+-- between "\\begin{" '}' "\\begin{env"  == Just "env"
+-- @
+betweenC'
+  :: Text -- ^ Prefix delimiter
+  -> Char -- ^ Suffix delimiter
+  -> Text -- ^ The string.
+  -> Maybe Text -- ^ Text between begin and end
+betweenC' beg end txt = skipAfter beg txt >>= (return . T.takeWhile (/=end))
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,6 +10,7 @@ import qualified Test.Tasty as T
 import Libkst.Hash
 import Libkst.IO
 import Libkst.Monad
+import Libkst.Text.Parse
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Exception (bracket_)
@@ -50,6 +51,114 @@ hashSpec = parallel $ do
 
   it "hashes text correctly" $
     hashText' (T.pack str) `shouldBe` expected
+
+-- * Parse tests
+
+parseSpec :: Spec
+parseSpec = parallel $ do
+  describe "skipTo" $ do
+    it "skips to prefix" $ do
+       skipTo "<xml" "<p><xml></xml>" `shouldBe` "<xml></xml>"
+       skipTo "<xml" "<xml></xml>" `shouldBe` "<xml></xml>"
+
+    it "returns empty string when no pattern is found" $ do
+       skipTo "<xml" "<p></xml>" `shouldBe` ""
+       skipTo "<xml" "" `shouldBe` ""
+
+  describe "skipAfter" $ do
+    it "skips after prefix" $ do
+       skipAfter "<xml>" "<p><xml></xml>" `shouldBe` Just "</xml>"
+       skipAfter "<xml>" "<xml></xml>" `shouldBe` Just "</xml>"
+       skipAfter "\\begin{" "aaaa\\begin{env}bbbb" `shouldBe` Just "env}bbbb"
+
+    it "returns Nothing when no pattern is found" $ do
+       skipAfter "<xml>" "</xml>" `shouldBe` Nothing
+       skipAfter "\\begin{" "aaaa\\end{env}bbbb" `shouldBe` Nothing
+       skipAfter "\\begin{" "" `shouldBe` Nothing
+
+  describe "takeUntil" $ do
+    it "takes until suffix" $ do
+       takeUntil "</xml>" "<p><xml></xml></p>" `shouldBe` "<p><xml>"
+       takeUntil "</xml>" "<p><xml></xml>" `shouldBe` "<p><xml>"
+       takeUntil "</xml>" "</xml></p>" `shouldBe` ""
+
+    it "returns entire string if suffix not found" $ do
+       takeUntil "</xml>" "<p><xml>" `shouldBe` "<p><xml>"
+       takeUntil "</xml>" "" `shouldBe` ""
+
+  describe "between'" $ do
+    it "finds string between begin and end" $ do
+       between' "<xml>" "</xml>" "aaaa<xml>text</xml>bbbb" `shouldBe` Just "text"
+       between' "<xml>" "</xml>" "aaaa<xml>text</xml>" `shouldBe` Just "text"
+       between' "<xml>" "</xml>" "<xml>text</xml>bbbb" `shouldBe` Just "text"
+       between' "<xml>" "</xml>" "<xml>text</xml>" `shouldBe` Just "text"
+       between' "\\begin{" "}" "\\begin{env}\\end{env}" `shouldBe` Just "env"
+       between' "\\begin{" "}" "aaaa\\begin{env}\\end{env}bbbb" `shouldBe` Just "env"
+       between' "\\begin{" "}" "aaaa\\begin{env}" `shouldBe` Just "env"
+
+    it "returns Nothing prefix not found" $ do
+       between' "\\begin{" "}" "\\end{env}" `shouldBe` Nothing
+       between' "\\begin{" "}" "\\end{env}" `shouldBe` Nothing
+       between' "\\begin{" "}" "aaaa\\end{env}" `shouldBe` Nothing
+       between' "\\begin{" "}" "env}" `shouldBe` Nothing
+
+    it "returns string between if suffix not found" $ do
+       between' "\\begin{" "}" "aaaa\\begin{env" `shouldBe` Just "env"
+       between' "\\begin{" "}" "\\begin{env{" `shouldBe` Just "env{"
+       between' "<xml>" "</xml>" "<xml>text<xml>" `shouldBe` Just "text<xml>"
+
+  describe "betweenC'" $ do
+    it "finds string between begin and end" $ do
+       betweenC' "\\begin{" '}' "\\begin{env}\\end{env}" `shouldBe` Just "env"
+       betweenC' "\\begin{" '}' "aaaa\\begin{env}\\end{env}bbbb" `shouldBe` Just "env"
+       betweenC' "\\begin{" '}' "aaaa\\begin{env}" `shouldBe` Just "env"
+
+    it "returns Nothing prefix not found" $ do
+       betweenC' "\\begin{" '}' "\\end{env}" `shouldBe` Nothing
+       betweenC' "\\begin{" '}' "\\end{env}" `shouldBe` Nothing
+       betweenC' "\\begin{" '}' "aaaa\\end{env}" `shouldBe` Nothing
+       betweenC' "\\begin{" '}' "env}" `shouldBe` Nothing
+
+    it "returns string between if suffix not found" $ do
+       betweenC' "\\begin{" '}' "aaaa\\begin{env" `shouldBe` Just "env"
+       betweenC' "\\begin{" '}' "\\begin{env{" `shouldBe` Just "env{"
+
+  describe "between" $ do
+    it "finds string between begin and end" $ do
+       between "<xml>" "</xml>" "aaaa<xml>text</xml>bbbb" `shouldBe` Just "text"
+       between "<xml>" "</xml>" "aaaa<xml>text</xml>" `shouldBe` Just "text"
+       between "<xml>" "</xml>" "<xml>text</xml>bbbb" `shouldBe` Just "text"
+       between "<xml>" "</xml>" "<xml>text</xml>" `shouldBe` Just "text"
+       between "\\begin{" "}" "\\begin{env}\\end{env}" `shouldBe` Just "env"
+       between "\\begin{" "}" "aaaa\\begin{env}\\end{env}bbbb" `shouldBe` Just "env"
+       between "\\begin{" "}" "aaaa\\begin{env}" `shouldBe` Just "env"
+
+    it "returns Nothing prefix not found" $ do
+       between "\\begin{" "}" "\\end{env}" `shouldBe` Nothing
+       between "\\begin{" "}" "\\end{env}" `shouldBe` Nothing
+       between "\\begin{" "}" "aaaa\\end{env}" `shouldBe` Nothing
+       between "\\begin{" "}" "env}" `shouldBe` Nothing
+
+    it "returns Nothing between if suffix not found" $ do
+       between "\\begin{" "}" "aaaa\\begin{env" `shouldBe` Nothing
+       between "\\begin{" "}" "\\begin{env{" `shouldBe` Nothing
+       between "<xml>" "</xml>" "<xml>text<xml>" `shouldBe` Nothing
+
+  describe "betweenC" $ do
+    it "finds string between begin and end" $ do
+       betweenC "\\begin{" '}' "\\begin{env}\\end{env}" `shouldBe` Just "env"
+       betweenC "\\begin{" '}' "aaaa\\begin{env}\\end{env}bbbb" `shouldBe` Just "env"
+       betweenC "\\begin{" '}' "aaaa\\begin{env}" `shouldBe` Just "env"
+
+    it "returns Nothing prefix not found" $ do
+       betweenC "\\begin{" '}' "\\end{env}" `shouldBe` Nothing
+       betweenC "\\begin{" '}' "\\end{env}" `shouldBe` Nothing
+       betweenC "\\begin{" '}' "aaaa\\end{env}" `shouldBe` Nothing
+       betweenC "\\begin{" '}' "env}" `shouldBe` Nothing
+
+    it "returns Nothing if suffix not found" $ do
+       betweenC "\\begin{" '}' "aaaa\\begin{env" `shouldBe` Nothing
+       betweenC "\\begin{" '}' "\\begin{env{" `shouldBe` Nothing
 
 -- * IO tests
 
@@ -117,10 +226,12 @@ ioSpec2 = around_ withTestDir $
 main :: IO ()
 main = do
   testHash <- testSpec "Hashing" hashSpec
+  testParse <- testSpec "Parsing" parseSpec
   testIO1 <- testSpec "IO1" ioSpec1
   testIO2 <- testSpec "IO2" ioSpec2
   defaultMain $ testGroup "Tests"
     [ testHash
+    , testParse
     , testIO1
-    , T.after AllFinish "IO1" testIO2 -- can't be run in parallel
+    , T.after AllFinish "IO1" testIO2 -- can't be run in parallel, TODO switch to MVar instead
     ]
